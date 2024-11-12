f7f74ef69c69775fb960d8fa83ba925609933af1
Sanjay V <sanjayv0521@gmail.com>
2024-11-12 19:31:44 +0530

    Update -> Singly Linked List

---
+++ src/singly_linked_list/singly.h
@@ -0,0 +1,10 @@
+#ifndef SINGLY_H
+#define SINGLY_H
+LNode* createNode(int data);
+void insertEnd(LNode *head, int data);
+LNode* insertStart(LNode *head, int data);
+int insertAfter(LNode *head, int insert_after, int data);
+void deleteEnd(LNode *head);
+void deleteStart(LNode *head);
+int deleteAfter(LNode *head, int delete_after);
+#endif // !SINGLY_H
