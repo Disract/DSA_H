cf2f8e1962282d193efa9f5741663ea22ffdca61
Sanjay V <sanjayv0521@gmail.com>
2024-11-12 11:07:08 +0530

    Update -> Doubly Linked List

--- src/doubly_linked_list/doubly.c
+++ src/doubly_linked_list/doubly.c
@@ -1,41 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct DNode DNode;
-struct DNode{
+
+typedef struct DNode{
   int data;
-  struct DNode *prev;
-  struct DNode *next;
+  DNode *prev;
+  DNode *next;
 }DNode;
 
-struct DNode* createNode(int data){
-  struct DNode* temp = (struct DNode*) malloc(sizeof(struct DNode));
+DNode* createNode(int data){
+  DNode* temp = (DNode*) malloc(sizeof(DNode));
   temp -> prev = NULL;
   temp -> next = NULL;
   temp -> data = data;
   return temp;
 }
 
-void insertAtEnd(struct DNode *head, int data){
-  struct DNode *ptr = head;
+void insertAtEnd(DNode *head, int data){
+  DNode *ptr = head;
   while(ptr -> next != NULL){
     ptr = ptr -> next;
   }
-  struct DNode *temp = createNode(data);
+  DNode *temp = createNode(data);
   ptr -> next = temp;
   temp -> prev = ptr;
 }
 
 
-struct DNode* insertAtBeg(struct DNode *head, int data){
-  struct DNode *temp = createNode(data);
+DNode* insertAtBeg(DNode *head, int data){
+  DNode *temp = createNode(data);
   temp -> next = head;
   head = temp;
   return head;
 }
 
 
-void insertAtMid(struct DNode *head, int insert_after, int data){
-  struct DNode *ptr = head;
+void insertAtMid(DNode *head, int insert_after, int data){
+  DNode *ptr = head;
   while(ptr != NULL && ptr -> data != insert_after){
     ptr = ptr -> next;
   }
@@ -43,8 +45,8 @@ void insertAtMid(struct DNode *head, int insert_after, int data){
     printf("Cannot insert after element %d : %d does not exist in the dl\n", insert_after, insert_after);
   }
   else{
-    struct DNode *temp = createNode(data);
-    struct DNode *ptr2 = ptr -> next;
+    DNode *temp = createNode(data);
+    DNode *ptr2 = ptr -> next;
     temp -> prev = ptr;
     temp -> next = ptr -> next;
     temp -> data = data;
@@ -55,8 +57,8 @@ void insertAtMid(struct DNode *head, int insert_after, int data){
 
 
 
-void printDL(struct DNode *head){
-  struct DNode *ptr = head;
+void printDL(DNode *head){
+  DNode *ptr = head;
   while(ptr != NULL){
     printf("value: %d next address: 0x%x \n" , ptr -> data, ptr -> next);
     ptr = ptr -> next;
