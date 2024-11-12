37c34222a59251c14ca38432bd724262d6ca24f3
Sanjay V <sanjayv0521@gmail.com>
2024-11-12 19:31:07 +0530

    Update -> Singly Linked List

--- src/singly_linked_list/singly.c
+++ src/singly_linked_list/singly.c
@@ -31,3 +31,45 @@ LNode* insertStart(LNode *head, int data){
   return temp;
 }
 
+int insertAfter(LNode *head, int insert_after, int data){
+  LNode *temp = head;
+  while (temp -> data != insert_after && temp != NULL) {
+    temp = temp -> next;
+  }
+  if(temp == NULL){
+    return  -1;
+  }
+  else{
+    LNode *node = createNode(data);
+    node -> next = temp -> next;
+    temp -> next = node;
+    return 0;
+  }
+}
+
+void deleteEnd(LNode *head){
+  LNode *temp = head;
+  while(temp -> next -> next != NULL)
+    temp = temp -> next;
+  temp -> next = NULL;
+}
+
+void deleteStart(LNode *head){
+  head = head -> next;
+}
+
+int deleteAfter(LNode *head, int delete_after){
+  LNode *temp = head;
+  LNode *prev_temp;
+  while(temp -> data != delete_after && temp != NULL){
+    prev_temp = temp;
+    temp = temp -> next;
+  }
+  if(temp == NULL)
+    return -1;
+  else {
+    prev_temp -> next = temp -> next;
+    temp = NULL;
+    return 0;
+  }
+}
